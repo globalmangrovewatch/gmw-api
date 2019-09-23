aca5201434c5b33a40d2fd4141b9db1affb463a4
David Inga <david.inga@vizzuality.com>
2019-09-23 11:02:29 +0200

    serializing with has_many instead

--- app/serializers/mangrove_datum_serializer.rb
+++ app/serializers/mangrove_datum_serializer.rb
@@ -2,6 +2,6 @@ class MangroveDatumSerializer < ActiveModel::Serializer
   attributes :id, :date, :gain_m2, :loss_m2, :net_change_m2, :length_m, :area_m2, :hmax_m, :agb_mgha_1, :hba_m, :con_hotspot_summary_km2
 
   def con_hotspot_summary_km2
-    JSON.parse self.object.con_hotspot_summary_km2
+    JSON.parse self.object.con_hotspot_summary_km2 if self.object.con_hotspot_summary_km2
   end
 end
