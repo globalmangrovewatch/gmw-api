09d6902980d190da0920bae34c3e062e737c1cfc
David Inga <david.inga@vizzuality.com>
2019-09-30 12:50:14 +0200

    fixing serializer for locations

--- app/serializers/mangrove_datum_serializer.rb
+++ app/serializers/mangrove_datum_serializer.rb
@@ -1,19 +1,19 @@
 class MangroveDatumSerializer < ActiveModel::Serializer
-  attributes :id, :date, :gain_m2, :loss_m2, :net_change_m2, :length_m, :area_m2, :hmax_m, :agb_mgha_1, :hba_m, :con_hotspot_summary_km2
+  attributes :id, :date, :gain_m2, :loss_m2, :net_change_m2, :length_m, :area_m2, :hmax_m, :agb_mgha_1, :hba_m, :con_hotspot_summary_km2, :agb_hist_mgha_1, :hba_hist_m, :hmax_hist_m
 
   def con_hotspot_summary_km2
-    JSON.parse self.object.con_hotspot_summary_km2 if self.object.con_hotspot_summary_km2
+    JSON.parse(self.object.con_hotspot_summary_km2) if self.object.con_hotspot_summary_km2
   end
 
   def agb_hist_mgha_1
-    JSON.parse self.object.agb_hist_mgha_1 if self.object.agb_hist_mgha_1
+    JSON.parse(self.object.agb_hist_mgha_1) if self.object.agb_hist_mgha_1
   end
 
   def hba_hist_m
-    JSON.parse self.object.hba_hist_m if self.object.hba_hist_m
+    JSON.parse(self.object.hba_hist_m) if self.object.hba_hist_m
   end
 
   def hmax_hist_m
-    JSON.parse self.object.hmax_hist_m if self.object.hmax_hist_m
+    JSON.parse(self.object.hmax_hist_m) if self.object.hmax_hist_m
   end
 end
