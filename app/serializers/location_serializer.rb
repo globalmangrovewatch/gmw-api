ab2b81f3ddd6863e34f925506f1527b03fddc280
David Inga <david.inga@vizzuality.com>
2020-07-09 12:33:33 +0200

    added geometry to location api

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,5 +1,5 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location_type, :iso, :bounds, :area_m2, :perimeter_m, :coast_length_m, :location_id
+  attributes :id, :name, :location_type, :iso, :bounds, :area_m2, :perimeter_m, :coast_length_m, :location_id, :geometry
 
   def bounds
     JSON.parse self.object.bounds if self.object.bounds
