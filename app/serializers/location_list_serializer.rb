0ad10383ce753df5a685e8563b24ecbc88816b80
David Inga <david.inga@vizzuality.com>
2021-08-17 09:52:01 +0200

    locations endpoint lighter

--- app/serializers/location_list_serializer.rb
+++ app/serializers/location_list_serializer.rb
@@ -1,5 +1,3 @@
 class LocationListSerializer < ActiveModel::Serializer
   attributes :id, :name, :location_type, :iso, :bounds, :area_m2, :perimeter_m, :coast_length_m, :location_id
-
-  has_many :mangrove_datum
 end
