ece2b70c7c475b912cf318f061a1e15e777bf93c
David Inga <david.inga@vizzuality.com>
2021-12-22 16:19:00 +0100

    migrations to version 7

--- db/migrate/20190805095138_add_location_id_to_locations.rb
+++ db/migrate/20190805095138_add_location_id_to_locations.rb
@@ -1,4 +1,4 @@
-class AddLocationIdToLocations < ActiveRecord::Migration[5.2]
+class AddLocationIdToLocations < ActiveRecord::Migration[7.0]
   def change
     add_column :locations, :location_id, :string
   end
--- db/migrate/20200715164255_add_total_carbon_columns_to_mangrove_data.rb
+++ db/migrate/20200715164255_add_total_carbon_columns_to_mangrove_data.rb
@@ -1,4 +1,4 @@
-class AddTotalCarbonColumnsToMangroveData < ActiveRecord::Migration[5.2]
+class AddTotalCarbonColumnsToMangroveData < ActiveRecord::Migration[7.0]
   def change
     add_column :mangrove_data, :agb_tco2e, :float
     add_column :mangrove_data, :bgb_tco2e, :float
