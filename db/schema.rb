b3623fd123047a4f9592fcdf17ad425898ecd1ab
Alicia <alicia.arenzana@gmail.com>
2022-06-23 11:36:47 +0200

    hot fixed the issue with value as a string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_22_100746) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_23_091833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,9 +70,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_100746) do
   create_table "international_statuses", force: :cascade do |t|
     t.bigint "location_id", null: false
     t.string "indicator"
-    t.float "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "value", null: false
     t.index ["location_id"], name: "index_international_statuses_on_location_id"
   end
 
