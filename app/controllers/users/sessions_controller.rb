8dfb8cdb13bf2f712d14f56d5ac5bd747a5e44d2
Angelo Arboleda <ghelobytes@gmail.com>
2022-06-23 08:16:54 -0700

    Updated auth controllers

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,23 +1,24 @@
 class Users::SessionsController < Devise::SessionsController
-    respond_to :json
-  
-    private
-  
-    def respond_with(resource, _opts = {})
-      render json: { message: 'You are logged in.' }, status: :ok
-    end
-  
-    def respond_to_on_destroy
-      log_out_success && return if current_user
-  
-      log_out_failure
-    end
-  
-    def log_out_success
-      render json: { message: "You are logged out." }, status: :ok
-    end
-  
-    def log_out_failure
-      render json: { message: "Hmm nothing happened."}, status: :unauthorized
-    end
+  skip_before_action :verify_authenticity_token
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: { message: "You are logged in." }, status: :ok
   end
+
+  def respond_to_on_destroy
+    log_out_success && return if current_user
+
+    log_out_failure
+  end
+
+  def log_out_success
+    render json: { message: "You are logged out." }, status: :ok
+  end
+
+  def log_out_failure
+    render json: { message: "Something went wrong"}, status: :unauthorized
+  end
+end
