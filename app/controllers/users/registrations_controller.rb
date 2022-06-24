84606a01ef093a05b165e610dc013db185a6d6e1
Angelo Arboleda <ghelobytes@gmail.com>
2022-06-24 06:15:40 -0700

    Handle non unique email

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   skip_before_action :verify_authenticity_token
   respond_to :json
 
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
+
   private
+
+  def record_not_unique(exception)
+    value = exception.message.split(")=(")[1].split(")")[0]
+    render json: { "error": "'%s' already exists." % [value] }, status: :unprocessable_entity
+  end
 
   def respond_with(resource, _opts = {})
     register_success && return if resource.persisted?
