00b98fa7a0c6bf623314390e48effaf420d7e057
Angelo Arboleda <ghelobytes@gmail.com>
2022-07-04 11:56:06 -0700

    Added admin flag

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          :registerable,
          jwt_revocation_strategy: JwtDenylist
+  def jwt_payload
+    {
+      'meta': {
+        'id': id,
+        'email': email,
+        'name': name,
+        'admin': admin
+      }
+    }
+  end
 end
