d21b40fb64598a50a59e567468e159e18badefe4
Angelo Arboleda <ghelobytes@gmail.com>
2022-08-15 12:13:17 -0700

    Updated messaging when changing/reseting password

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,6 +1,6 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
-    ENV["USER_CONFIRMATION_REDIRECT_URL"]
+    ENV["MRTT_UI_BASE_URL"] + "/auth/login"
   end
 end
