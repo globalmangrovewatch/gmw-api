79d7a1903580a70492d00619e054fefbfd2aadff
Angelo Arboleda <ghelobytes@gmail.com>
2022-08-16 12:13:08 -0700

    Return immediately after redirect

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -2,6 +2,6 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
     url = ENV["MRTT_UI_BASE_URL"] + "/auth/login"
-    redirect_to  url, allow_other_host: true
+    redirect_to(url, allow_other_host: true) && return
   end
 end
