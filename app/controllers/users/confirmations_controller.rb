06f6182fe477fb18193c99c87532601dc8bd3169
Angelo Arboleda <ghelobytes@gmail.com>
2022-08-16 12:09:13 -0700

    Allow redirection to external url

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,6 +1,7 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
-    ENV["MRTT_UI_BASE_URL"] + "/auth/login"
+    url = ENV["MRTT_UI_BASE_URL"] + "/auth/login"
+    redirect_to  url, allow_other_host: true
   end
 end
