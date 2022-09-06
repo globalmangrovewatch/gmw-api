3b7b7c9af184900228fb24bce92fc6ade09bbefb
Angelo Arboleda <ghelobytes@gmail.com>
2022-09-06 12:04:47 -0700

    Return just the centroid (work-around until postgis is enabled)

--- app/controllers/v2/dashboard_controller.rb
+++ app/controllers/v2/dashboard_controller.rb
@@ -5,6 +5,7 @@ class V2::DashboardController < MrttApiController
         site_area_question_id = "1.3"
         site_area_clause = "LEFT JOIN registration_answers ON " +
                            "(sites.id = registration_answers.site_id and registration_answers.question_id = '%s')" % site_area_question_id
-        @sites = Site.left_joins(:landscape).joins(site_area_clause).select("sites.*, registration_answers.answer_value as site_area")
+        site_centroid = "registration_answers.answer_value::json->'features'->0->'geometry'->'coordinates'->0->0 as site_centroid"
+        @sites = Site.left_joins(:landscape).joins(site_area_clause).select("sites.*, %s" % site_centroid)
     end
 end
