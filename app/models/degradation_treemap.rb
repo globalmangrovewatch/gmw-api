7166de1a5c94c4071424dd782cb18ed2da4432c8
Alicia <alicia.arenzana@gmail.com>
2022-09-09 16:04:03 +0200

    updated treemap

--- app/models/degradation_treemap.rb
+++ app/models/degradation_treemap.rb
@@ -1,3 +1,10 @@
 class DegradationTreemap < ApplicationRecord
   belongs_to :location
+
+  enum indicator: {
+    "degraded_area":"degraded_area",
+    "lost_area":"lost_area",
+    "mangrove_area":"mangrove_area",
+    "restorable_area":"restorable_area",
+    }
 end
