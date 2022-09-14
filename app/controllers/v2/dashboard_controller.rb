38a413d5298fcff02b2e1a008cca234ee30f883c
Angelo Arboleda <ghelobytes@gmail.com>
2022-09-14 12:04:00 -0700

    Return the actual centroid of sites.area geom

--- app/controllers/v2/dashboard_controller.rb
+++ app/controllers/v2/dashboard_controller.rb
@@ -5,7 +5,7 @@ class V2::DashboardController < MrttApiController
         site_area_question_id = "1.3"
         site_area_clause = "LEFT JOIN registration_answers ON " +
                            "(sites.id = registration_answers.site_id and registration_answers.question_id = '%s')" % site_area_question_id
-        site_centroid = "registration_answers.answer_value::json->'features'->0->'geometry'->'coordinates'->0->0 as site_centroid"
+        site_centroid = "ST_AsGeoJSON(ST_Centroid(sites.area)) as site_centroid"
         @sites = Site.left_joins(:landscape).joins(site_area_clause).select("sites.*, %s" % site_centroid)
     end
 end
