039dc6f3d0a7137a3aa1cf08fdb2dd7568abd66a
Angelo Arboleda <ghelobytes@gmail.com>
2022-09-26 08:05:28 -0700

    Remove auth for now

--- app/controllers/v2/report_controller.rb
+++ app/controllers/v2/report_controller.rb
@@ -1,5 +1,5 @@
 class V2::ReportController < MrttApiController
-    # skip_before_action :authenticate_user!
+    skip_before_action :authenticate_user!
 
     def answers
         range_query = "split_part(question_id, '.', 1)::numeric between 1 and 5"
