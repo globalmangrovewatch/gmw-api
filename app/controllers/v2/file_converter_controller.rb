ed5304c7e50b52ad456272f5e2baa0b4e8155a69
Alicia <alicia.arenzana@gmail.com>
2022-09-29 16:16:10 +0200

    file limit setup

--- app/controllers/v2/file_converter_controller.rb
+++ app/controllers/v2/file_converter_controller.rb
@@ -1,11 +1,11 @@
 class V2::FileConverterController < ApiController
     include FileDataImport
-    MAX_FILE_SIZE = 10000000000
+    MAX_FILE_SIZE = 20000000
     # POST /spatial_file/converter
     def convert
         file = import_params[:file]
         @response = if file.blank? || file.size > MAX_FILE_SIZE
-            exception("File must exist and be smaller than #{MAX_FILE_SIZE/1000} KB")
+            exception("File must exist and be smaller than #{MAX_FILE_SIZE/1000000} MB")
             else
                 importer.convert
             end
