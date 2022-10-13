c69d022ec2ebde5fb394add3ce6b33bef30fe3d1
Angelo Arboleda <ghelobytes@gmail.com>
2022-10-13 12:36:49 -0700

    Added monitoring answers to reports

--- app/controllers/v2/report_controller.rb
+++ app/controllers/v2/report_controller.rb
@@ -1,13 +1,28 @@
 class V2::ReportController < MrttApiController
     skip_before_action :authenticate_user!
 
-    def answers
-        if report_params[:site_id]
-            @answers = RegistrationAnswer.where(site_id: report_params[:site_id])
-        else
-            @answers = RegistrationAnswer.all
-        end
-        puts report_params
+    def answers_by_site
+        site_id = report_params[:site_id]
+        @site = Site.find(site_id)
+        @registration_intervention_answers = @site.registration_intervention_answers
+
+        monitoring_events = {}
+        @site.monitoring_answers.each { |answer|
+            if not monitoring_events.key?(answer.uuid)
+                monitoring_events[answer.uuid] = {
+                    "uuid" => answer.uuid,
+                    "form_type" => answer.form_type,
+                    "monitoring_date" => answer.monitoring_date,
+                    "answers" => {}
+                }
+            end
+            monitoring_events[answer.uuid]["answers"][answer.question_id] = answer.answer_value
+        }
+
+        @monitoring_answers = []
+        monitoring_events.each { |key, value|
+            @monitoring_answers.push(value)
+        }
     end
 
     def report_params
--- app/views/v2/report/answers.jbuilder
+++
@@ -1,5 +0,0 @@
-json.array! @answers do |answer|
-  json.site_id answer[:site_id]
-  json.question_id answer[:question_id]
-  json.answer_value answer[:answer_value]
-end
