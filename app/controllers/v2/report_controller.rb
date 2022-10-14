7d65a5ef4bbfb9a7ef51c0023aff4f8bc59c05a3
Angelo Arboleda <ghelobytes@gmail.com>
2022-10-14 08:51:08 -0700

    Feature/reporting with monitoring (#120)
    * Added monitoring answers to reports
    
    * Added endpoint for all sites
    
    * Remove .unotes

--- app/controllers/v2/report_controller.rb
+++ app/controllers/v2/report_controller.rb
@@ -1,13 +1,45 @@
 class V2::ReportController < MrttApiController
     skip_before_action :authenticate_user!
 
+    def answers_by_site
+        site_id = report_params[:site_id]
+        @site_id, @registration_intervention_answers, @monitoring_answers = get_answers_by_site(site_id)
+    end
+
     def answers
-        if report_params[:site_id]
-            @answers = RegistrationAnswer.where(site_id: report_params[:site_id])
-        else
-            @answers = RegistrationAnswer.all
-        end
-        puts report_params
+        @answers = []
+        Site.all.each { |site|
+            site_id, registration_intervention_answers, monitoring_answers = get_answers_by_site(site.id)
+            @answers.push({
+                "site_id" => site_id,
+                "registration_intervention_answers" => registration_intervention_answers,
+                "monitoring_answers" => monitoring_answers
+            })
+        }
+    end
+
+    def get_answers_by_site(site_id)
+        site = Site.find(site_id)
+        registration_intervention_answers = site.registration_intervention_answers
+
+        monitoring_events = {}
+        site.monitoring_answers.each { |answer|
+            if not monitoring_events.key?(answer.uuid)
+                monitoring_events[answer.uuid] = {
+                    "uuid" => answer.uuid,
+                    "form_type" => answer.form_type,
+                    "monitoring_date" => answer.monitoring_date,
+                    "answers" => {}
+                }
+            end
+            monitoring_events[answer.uuid]["answers"][answer.question_id] = answer.answer_value
+        }
+
+        monitoring_answers = []
+        monitoring_events.each { |key, value|
+            monitoring_answers.push(value)
+        }
+        return site.id, registration_intervention_answers, monitoring_answers
     end
 
     def report_params
--- app/views/v2/report/answers.jbuilder
+++ app/views/v2/report/answers.jbuilder
@@ -1,5 +1,7 @@
 json.array! @answers do |answer|
-  json.site_id answer[:site_id]
-  json.question_id answer[:question_id]
-  json.answer_value answer[:answer_value]
+  json.site_id answer["site_id"]
+  json.registration_intervention_answers do
+    answer["registration_intervention_answers"].each{ |answer| json.set! answer.question_id, answer.answer_value }
+  end
+  json.monitoring_answers answer["monitoring_answers"]
 end
