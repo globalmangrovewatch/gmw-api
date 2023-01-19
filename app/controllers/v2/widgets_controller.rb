c77bfeebbca1b10f817c255766c23d288975d145
Alicia <alicia.arenzana@gmail.com>
2023-01-19 11:13:20 +0100

    added conversion

--- app/controllers/v2/widgets_controller.rb
+++ app/controllers/v2/widgets_controller.rb
@@ -1,22 +1,15 @@
 include PivotTableHelper
 include ApplicationHelper
+
 class V2::WidgetsController < ApiController
-  
-   # GET /v2/widgets/protected-areas
+
+  # GET /v2/widgets/protected-areas
   def protected_areas
     # original area data is in hectares, 
     # but we want to display it in square km 
     # TODO: make this configurable https://github.com/mhuggins/ruby-measurement
-    @conversion_factor = 1
+    @conversion_factor = convert_factor(params)
     @unit = params[:units] || 'ha'
-    if params.has_key?(:units)
-      @conversion_factor = case params[:units]
-        when 'km2' then 0.001
-        when 'ha' then  1
-        when 'm' then  10000
-        else 1
-      end
-    end
     # location_id here references location.location_id instead of location.id
     if params.has_key?(:location_id) && params[:location_id] != 'worldwide'
       @location_id = params[:location_id]
@@ -96,15 +89,22 @@ class V2::WidgetsController < ApiController
       'protected' => 'Protected',
       'remaining' => 'Remaining',
     }
+    @conversion_factor = convert_factor(params)
+    @unit = params[:units] || 'ha'
+    
     if params.has_key?(:location_id) && params[:location_id] != 'worldwide'
       @location_id = params[:location_id]
-      @data = BlueCarbonInvestment.select('category, description, location_id, area, sum(area) over () as total_area').where(location_id: params[:location_id])
+      @data = BlueCarbonInvestment.select("category, (area*#{@conversion_factor})::varchar(255)\
+       ||' (± '|| area*0.05*#{@conversion_factor} || ')' as description, location_id, area,\
+       sum(area) over () as total_area").where(location_id: @location_id)
     else
-      @data = BlueCarbonInvestment.select("category, area::varchar(255) || ' (±'|| area*0.05 || ')' as description, area, sum(area) over () as total_area"
+      @location_id = 'worldwide'
+      @data = BlueCarbonInvestment.select("category, (area*#{@conversion_factor})::varchar(255)\
+       || ' (±'|| area*0.05*#{@conversion_factor} || ')' as description,'_' as description_a, area,\
+        sum(area) over () as total_area"
     ).from(BlueCarbonInvestment.select('category, sum("blue_carbon_investments".area) as area'
           ).group('category'), :a
     )
-      @location_id = 'worldwide'
     end
   end
 
@@ -349,5 +349,20 @@ class V2::WidgetsController < ApiController
       ).from(subquery).group(:name, :indicator, :iso
       ).order('2 desc'
       ).limit(@limit)
+    end
+
+    private
+    def convert_factor(params)
+      logger.info "asdfasdf"
+      logger.error "asdfasdf"
+      if params.has_key?(:units)
+        @conversion_factor = case params[:units]
+          when 'km2' then 0.001
+          when 'ha' then  1
+          when 'm' then  10000
+          else 1
+        end
+      else @conversion_factor = 1
+      end  
     end
 end
--- app/views/v2/widgets/blue_carbon_investment.json.jbuilder
+++ app/views/v2/widgets/blue_carbon_investment.json.jbuilder
@@ -1,16 +1,15 @@
 json.data do
   json.array! @data do |datum|
     json.category datum.category
-    json.value datum.area
+    json.value datum.area * @conversion_factor
     json.percentage datum.area / datum.total_area * 100
     json.description datum.description
     json.label @labels[datum.category]
-
   end
 end
 
 json.metadata do
-  json.location_id  @location_id
-  json.unit 'ha'
+  json.location_id @location_id
+  json.unit @unit
   json.note nil
 end
