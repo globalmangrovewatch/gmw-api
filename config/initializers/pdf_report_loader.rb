46d40798ef37291016c639ec1e38d3c87ba048d4
silentunion <reecemilliner@gmail.com>
2023-06-23 09:07:45 -0700

    PDFKit now takes in section and subsection

--- config/initializers/pdf_report_loader.rb
+++ config/initializers/pdf_report_loader.rb
@@ -4,8 +4,8 @@
 #     config.pdf_format = PDFReportFormatter.pdf_report_formatter
 # end
 
-PDFKit.configure do |config|
-    config.default_options = {
-      footer_right: "[page]"
-    }
-  end
+# PDFKit.configure do |config|
+#     config.default_options = {
+#       footer_right: "[page]"
+#     }
+#   end
