2b538e0ef2f394759564addae7475c27eded57b7
silentunion <reecemilliner@gmail.com>
2023-06-26 13:11:17 -0700

    Linting create_admin

--- db/create_admin.rb
+++ db/create_admin.rb
@@ -7,26 +7,26 @@ password = ARGV[2]
 
 email_exists = User.exists?(email: email)
 if email_exists
-    puts "The email %s already exists." % email
-    exit
+  puts "The email %s already exists." % email
+  exit
 end
 
 admin = User.new({
-        name: name,
-        email: email,
-        password: password,
-        password_confirmation: password,
-    })
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+  })
 admin.toggle!(:admin)
 
 if admin.valid?
-    admin.save()
+  admin.save()
 elsif admin.errors.any?
-    admin.errors.full_messages.each do |msg|
-    puts msg
-    end
+  admin.errors.full_messages.each do |msg|
+  puts msg
+  end
 else
-    puts "Unknown error happened while creating an admin user."
+  puts "Unknown error happened while creating an admin user."
 end
 
 puts "Admin user created"
