14b6f743d3be689232b427ca8ea69193af338762
leo guercio <lpguercio@gmail.com>
2026-01-23 12:08:30 +0100

    Add proper file discovery for pdf stuff

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,4 +1,12 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf = `which wkhtmltopdf`.to_s.strip
+  config.wkhtmltopdf = if File.exist?('/app/.apt/usr/bin/wkhtmltopdf')
+    '/app/.apt/usr/bin/wkhtmltopdf'
+  elsif File.exist?('/usr/bin/wkhtmltopdf')
+    '/usr/bin/wkhtmltopdf'
+  elsif File.exist?('/usr/local/bin/wkhtmltopdf')
+    '/usr/local/bin/wkhtmltopdf'
+  else
+    `which wkhtmltopdf`.to_s.strip
+  end
 end
 
