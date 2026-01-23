8e8c4e44f63f4cd5e3d6e3d3773570c0878a435d
leo guercio <lpguercio@gmail.com>
2026-01-23 15:53:35 +0100

    Another attempt to fix pdf

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,10 +1,10 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf = if File.exist?('/app/.apt/usr/bin/wkhtmltopdf')
-    '/app/.apt/usr/bin/wkhtmltopdf'
-  elsif File.exist?('/usr/bin/wkhtmltopdf')
-    '/usr/bin/wkhtmltopdf'
+  config.wkhtmltopdf = if File.exist?('/app/bin/wkhtmltopdf')
+    '/app/bin/wkhtmltopdf'
   elsif File.exist?('/usr/local/bin/wkhtmltopdf')
     '/usr/local/bin/wkhtmltopdf'
+  elsif File.exist?('/usr/bin/wkhtmltopdf')
+    '/usr/bin/wkhtmltopdf'
   else
     `which wkhtmltopdf`.to_s.strip
   end
